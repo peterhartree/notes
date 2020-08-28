9c88a3c7c98723d730c811412938c65a4d5c3bdf
Peter Hartree <web@peterhartree.co.uk>
2020-08-28 13:27:48 +0100

    Initial commit
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

---
+++ src/.vuepress/config.js
@@ -0,0 +1,83 @@
+const { description } = require('../../package')
+const glob = require('glob');
+
+let markdownFiles = glob.sync('docs/**/*.md').map(f => '/' + f);
+// update the docs/**/*.md pattern with your folder structure
+
+console.log('markdown files');
+console.log(markdownFiles);
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Peter working\'s notes',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'People',
+        link: '/people/people.md'
+      },
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      }
+    ],
+    sidebar: markdownFiles
+    /*
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+          */
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: {
+    '@vuepress/plugin-back-to-top': {},
+    '@vuepress/plugin-medium-zoom': {},
+    'sitemap': {
+      hostname: 'https://pake.web.id'
+    }
+  }
+}
---
+++ src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+# heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: Thinking with the garage door up.
+# actionText: People I've learned from →
+# actionLink: /people/people.md
+features:
+- title: People
+  details: An incomplete list of people I've learned from](/people/People.md).
+- title: Journal
+  details: Feature 3 Description
+- title: Podcasts
+  details: Feature 2 Description
+footer: Thoughts? Send me a message!
+---
